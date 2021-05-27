7fbb140ca317a13c12c60bfc17af0ff30b8e544c
GabrielJusto <gabriel.justo@acad.pucrs.br>
2021-05-26 23:43:37 -0300

    Pool Connection adicionado

--- loja-virtual-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,14 +1,29 @@
 package br.com.alura.jdbc.factory;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	
+	DataSource dataSource;
+	
+	public ConnectionFactory()
+	{
+		ComboPooledDataSource comboPoolDatasource = new ComboPooledDataSource();
+		comboPoolDatasource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPoolDatasource.setUser("root");
+		comboPoolDatasource.setPassword("root");
+	
+		comboPoolDatasource.setMaxPoolSize(10);
+		
+		this.dataSource = comboPoolDatasource;
+	}
+	
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager
-				.getConnection
-				("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-						"root", "root");
+		return this.dataSource.getConnection();
 	}
 }
